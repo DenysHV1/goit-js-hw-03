6c82da4e4a0a07f2409290b3eed2047d11c1bc04
DenVik <denyagarkusha2@gmail.com>
2024-06-21 20:07:33 +0200

    +pritter

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,17 @@
-function makeArray (firstArray, secondArray, maxLength) {
+function makeArray(firstArray, secondArray, maxLength) {
+	const allElements = firstArray.concat(secondArray);
+	const result = [];
 
-const allElements = firstArray.concat(secondArray);
-const result = [];
-
-	for (let i = 0; i < maxLength; i+=1){
-		result.push(allElements[i])
+	for (let i = 0; i < maxLength; i += 1) {
+		result.push(allElements[i]);
 	}
 
-return result
-
+	return result;
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
